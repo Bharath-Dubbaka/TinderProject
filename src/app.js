f40571c95f6e59ff784f91ef32100a5cdab8f7e6
Bharath Dubbaka <bharath.reactdev@gmail.com>
2025-05-01 17:46:44 +0530

    routing tests with params and other regex types

--- src/app.js
+++ src/app.js
@@ -4,17 +4,30 @@ const express = require("express");
 
 const app = express();
 
-app.use("/hello", (req, res) => {
-   res.send("hello");
+
+//getting data from URL params
+app.get("/user/:name/:email", (req, res) => {
+   console.log(req.params);
+   const params = req.params;
+   res.send({ params, from: "paramURL" });
 });
 
-app.use("/test", (req, res) => {
-   res.send("test");
-});
+//order of the routes do matter
+// app.use("/hello/2", (req, res) => {
+//    res.send("hello/2");
+// });
 
-app.use("/", (req, res) => {
-   res.send("Hello main page");
-});
+// app.use("/hello", (req, res) => {
+//    res.send("hello");
+// });
+
+// app.use("/test", (req, res) => {
+//    res.send("test");
+// });
+
+// app.use("/", (req, res) => {
+//    res.send("Hello main page");
+// });
 
 app.listen(9999, () => {
    console.log("express is up and running in port 9999");
