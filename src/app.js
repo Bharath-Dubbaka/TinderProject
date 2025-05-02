865b4f158514f9e6267d1a9722c3721f6ed4dbfa
Bharath Dubbaka <bharath.reactdev@gmail.com>
2025-05-02 16:59:18 +0530

    added new HTTP post and get methods with dynamic data

--- src/app.js
+++ src/app.js
@@ -1,6 +1,40 @@
 const express = require("express");
 const { connectDB } = require("./config/database");
 const app = express();
+const { User } = require("./models/user");
+
+app.use(express.json());
+
+app.get("/feed", async (req, res) => {
+   console.log("GET feed called", req.body);
+   const users = await User.find(req.body);
+   if (users.length === 0) {
+      res.status(404).send("No Users Found with given email", res.body);
+   } else {
+      console.log(users);
+      res.send(users);
+   }
+});
+
+app.post("/signup", async (req, res) => {
+   console.log("POST called", req.body);
+   //calling new instance of user to send data to DB via mongoose
+   // const user = new User({
+   //    firstName: "brat",
+   //    lastName: "bratName",
+   //    emailID: "brat@g.com",
+   //    password: "bratPass",
+   // });
+
+   //calling new instance of user to send data from request... to DB via mongoose
+   const user = new User(req.body);
+   try {
+      await user.save();
+      res.send("user added successfully");
+   } catch (err) {
+      res.status(400).send("user NOT added successfully");
+   }
+});
 
 connectDB()
    .then(() => {
