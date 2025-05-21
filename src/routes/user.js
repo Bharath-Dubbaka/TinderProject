002f21fb6badacf96f3eef63f9d39588b6e15c61
Bharath Dubbaka <bharath.reactdev@gmail.com>
2025-05-21 15:45:35 +0530

    added feed API AND also pagination using skip() and limit() fns from mongo

--- src/routes/user.js
+++ src/routes/user.js
@@ -87,4 +87,59 @@ userRouter.get("/user/requests/accepted", userAuth, async (req, res) => {
    }
 });
 
+userRouter.get("/feed", userAuth, async (req, res) => {
+   try {
+      const loggedInUser = req.user; // coming from middleware
+      const page = parseInt(req.query.page) || 1;
+      let limit = parseInt(req.query.limit) || 10;
+      limit = limit > 50 ? 50 : limit;
+      const skip = (page - 1) * limit;
+
+      //all users
+      // except: previously ignored "by" loggedInUser
+      // except: previously interested "by" loggedInUser
+      // except: already connected/accepted profiles
+      // except: profiles which ignored "the" loggedInUser
+      // except: his own loggedInUser profile
+
+      const connectionRequest = await ConnectionRequest.find({
+         $or: [
+            { fromUserID: loggedInUser._id },
+            { toUserID: loggedInUser._id },
+         ],
+      }).select("fromUserID toUserID");
+
+      const hideUsersFromFeed = new Set();
+
+      connectionRequest.forEach((req) => {
+         hideUsersFromFeed.add(req.toUserID.toString());
+         hideUsersFromFeed.add(req.fromUserID.toString());
+      });
+      console.log(hideUsersFromFeed, "hideUsersFromFeed");
+      console.log(connectionRequest, "connectionRequest");
+      // console.log(connectionRequest, "connectionRequest");
+      // console.log(feed, "connectionRequest");
+      // console.log(loggedInUser, "loggedInUser");
+
+      const filteredUsers = await User.find({
+         //Array.from(hideUsersFromFeed) to convert from Set to array
+         $and: [
+            { _id: { $nin: Array.from(hideUsersFromFeed) } },
+            { _id: { $ne: loggedInUser._id } },
+         ],
+      })
+         .select("firstName lastName photoUrl age about gender")
+         .skip(skip)
+         .limit(limit);
+
+      res.json({
+         message: "feed fetched successfully",
+         data: filteredUsers,
+      });
+   } catch (err) {
+      res.status(400).send(" feed NOT fetched :::" + err.message);
+      console.log(" feed NOT fetched", err.message);
+   }
+});
+
 module.exports = { userRouter };
